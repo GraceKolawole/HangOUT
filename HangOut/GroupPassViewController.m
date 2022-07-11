167389e7437aab6d6c0f1182c4f140eab0ebe887
Oluwanifemi Kolawole <gracekolawole@fb.com>
2022-07-11 10:13:30 -0700

    Add group buttons to GroupViewController.m

---
+++ HangOut/GroupPassViewController.m
@@ -0,0 +1,31 @@
+//
+//  GroupPassViewController.m
+//  HangOut
+//
+//  Created by Oluwanifemi Kolawole on 7/11/22.
+//
+
+#import "GroupPassViewController.h"
+
+@interface GroupPassViewController ()
+
+@end
+
+@implementation GroupPassViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

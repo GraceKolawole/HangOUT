a2b76b79d72d64140dfe65d104d690b74e4a8ab0
Oluwanifemi Kolawole <gracekolawole@fb.com>
2022-07-13 12:21:17 -0700

    Add posterImage to EventViewController.m adn reassigned constrants to features in EventViewContoller storyboard

--- HangOut/EventsViewController.m
+++ HangOut/EventsViewController.m
@@ -37,6 +37,7 @@
 
     [self.tableView addSubview:self.refreshControl];
     [self.tableView insertSubview:self.refreshControl atIndex:0];
+//    self.tableView.estimatedRowHeight = 257;
 }
 -(void) fetchEvents{
     NSURL *url= [NSURL URLWithString:@"https://api.seatgeek.com/2/events?client_id=Mjc3NjgxNTV8MTY1NzU1NDk5MC4zNjM1OTU3&client_secret=84710cd42677f14b657b6203e088a97a1bdc67637e7b9468ee2f53eb2a5ea894"
@@ -85,18 +86,19 @@
     cell.eventType.text = event[@"type"];
     cell.locationLabel.text = venue[@"display_location"];
     cell.dateTimeLabel.text = event[@"datetime_utc"];
-    //
-    //    NSString *baseURLString = @"https://seatgeek.com/images";
-    //    NSString *posterURLString = performers[0][@"image"];
-    //    NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+
+        NSString *posterURLString = performers[0][@"image"];
         
-    //    NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
-    //    cell.posterView.image = nil;
-    //    [posterURL saveInBackground];
-    //    [cell.posterView setImageWithURL:posterURL];
-        
-    //    NSURL *linkURL  = performers[0][@"url"];
-    //    NSURL *myURL = performers[0][[NSURL alloc]initWithString:@"https://seatgeek.com/performers/tickets"];
+        NSURL *posterURL = [NSURL URLWithString:posterURLString];
+    [NSData dataWithContentsOfURL:posterURL];
+    NSData *posterImageData = [NSData dataWithContentsOfURL:posterURL];
+    cell.posterView.image = [UIImage imageWithData:posterImageData];
+//        cell.posterView.image = nil;
+//        [posterURL saveInBackground];
+//        [cell.posterView setImageWithURL:posterURL];
+//
+//        NSURL *linkURL  = performers[0][@"url"];
+//        NSURL *myURL = performers[0][[NSURL alloc]initWithString:@"https://seatgeek.com/performers/tickets"];
     //    cell.linkLabel.text = myURL;
     //    NSString *  = performers[0][@"url"];
     //    NSData *myData = [[NSData alloc]initWithContentsOfURL:myURL];

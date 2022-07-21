7b48f921b2c00c553950e43de321dfc6b5dfa185
Grace Kolawole <107007242+GraceKolawole@users.noreply.github.com>
2022-07-21 09:06:24 -0700

    Implementing the searchbar and the fetchMoreEvent to fetch events while scrolling in EventViewContoller.m

--- HangOut/EventsViewController.m
+++ HangOut/EventsViewController.m
@@ -11,26 +11,33 @@
 #import "Parse/Parse.h"
 #import "SceneDelegate.h"
 
-
-//
-@interface EventsViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
+@interface EventsViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate, UISearchResultsUpdating, UISearchBarDelegate>
+{
+    int pageId;
+    NSString *searchText;
+}
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *events;
 @property (nonatomic, strong) UIRefreshControl *refreshControl;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator;
-
-
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (strong, nonatomic) NSArray *filteredEvent;
 //
 @end
 //
 @implementation EventsViewController
-//
+
 - (void)viewDidLoad {
+    pageId = 1;
+    searchText = nil;
     [super viewDidLoad];
+
+    [self.searchBar becomeFirstResponder];
+    self.searchBar.delegate = self;
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     self.tableView.rowHeight = UITableViewAutomaticDimension;
-    
+
     [self.indicator startAnimating];
     [self fetchEvents];
     self.refreshControl = [[UIRefreshControl alloc] init];
@@ -40,9 +47,10 @@
     [self.tableView insertSubview:self.refreshControl atIndex:0];
     [self.refreshControl endRefreshing];
 }
+
 -(void) fetchEvents{
-    NSURL *url= [NSURL URLWithString:@"https://api.seatgeek.com/2/events?client_id=Mjc3NjgxNTV8MTY1NzU1NDk5MC4zNjM1OTU3&client_secret=84710cd42677f14b657b6203e088a97a1bdc67637e7b9468ee2f53eb2a5ea894"
-                ];
+    self.filteredEvent = self.events;
+    NSURL *url= [NSURL URLWithString:@"https://api.seatgeek.com/2/events?client_id=Mjc3NjgxNTV8MTY1NzU1NDk5MC4zNjM1OTU3&client_secret=84710cd42677f14b657b6203e088a97a1bdc67637e7b9468ee2f53eb2a5ea894"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
@@ -52,36 +60,30 @@
            }
            else {
                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               NSLog(@"%@",  dataDictionary);
+               
                self.events = dataDictionary[@"events"];
-               
-            
+               self.filteredEvent = dataDictionary[@"events"];
+
                [self.tableView reloadData];
-               
            }
         [self.refreshControl endRefreshing];
         [self.indicator stopAnimating];
-        
     }];
     [task resume];
 }
+
 - (void)didReceiveMemoryWarning{[super didReceiveMemoryWarning];
     
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
-(NSInteger)section{
-    return self.events.count;
-}
-
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
-    EventCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EventCell"];
-    NSDictionary *event = self.events[indexPath.row];
+    EventCell *cell = [tableView dequeueReusableCellWithIdentifier:@"EventCell" forIndexPath:indexPath];
+    NSDictionary *event = self.filteredEvent[indexPath.row];
     NSDictionary *venue = event[@"venue"];
     NSArray *performers = event[@"performers"];
 
-    
-    
     cell.addressLabel.text = venue[@"address"];
     cell.cityLabel.text = venue[@"city"];
     cell.eventType.text = event[@"type"];
@@ -108,32 +110,120 @@
     NSData *posterImageData = [NSData dataWithContentsOfURL:posterURL];
     cell.posterView.image = [UIImage imageWithData:posterImageData];
         }
-        
     }
-
     return cell;
 }
+
+- (void)fetchMoreEvents{
+    pageId += 1;
+    NSString *urlString = [NSString stringWithFormat:@"https://api.seatgeek.com/2/events?client_id=Mjc3NjgxNTV8MTY1NzU1NDk5MC4zNjM1OTU3&client_secret=84710cd42677f14b657b6203e088a97a1bdc67637e7b9468ee2f53eb2a5ea894&per_page=25&page=%i", pageId];
+    NSURL *url= [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+               [self displayAlertMessages];
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               NSMutableArray *newEvents = [self.events mutableCopy];
+               [newEvents addObjectsFromArray:dataDictionary[@"events"]];
+               self.events = newEvents;
+               
+               [self.tableView reloadData];
+           }
+    }];
+    [task resume];
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    NSString *urlString = [NSString stringWithFormat:@"https://api.seatgeek.com/2/events?client_id=Mjc3NjgxNTV8MTY1NzU1NDk5MC4zNjM1OTU3&client_secret=84710cd42677f14b657b6203e088a97a1bdc67637e7b9468ee2f53eb2a5ea894&per_page=25&page=%i&q=%@", pageId, searchText];
+    NSURL *url= [NSURL URLWithString:urlString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+               [self displayAlertMessages];
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               self.filteredEvent = dataDictionary[@"events"];
+               
+               [self.tableView reloadData];
+           }
+        [self.refreshControl endRefreshing];
+        [self.indicator stopAnimating];
+    }];
+    [task resume];
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+  if (indexPath.row == self.events.count - 6) {
+    pageId += 1;
+    [self fetchEvents];
+  }
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:
+(NSInteger)section{
+    if (self.filteredEvent.count > 0){
+    return self.filteredEvent.count;
+    }
+    return self.events.count;
+}
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
   [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
+
 - (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
   // Disable selecting row until implemented
   return nil;
 }
 
--(void) displayAlertMessages{
+- (void) displayAlertMessages{
     UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Poor Connection" message:@"Check your internet connectiom and try again" preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {
         NSLog(@"OK");
         [self.indicator startAnimating];
         [self fetchEvents];
     }];
-
+    
     [alertController addAction:okAction];
     [self presentViewController:alertController animated:YES completion:nil];
 }
+
+-(void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
+    self.searchBar.showsCancelButton = YES;
+}
+
+-(void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    self.searchBar.showsCancelButton = NO;
+    self.searchBar.text = @"";
+    [self.searchBar resignFirstResponder];
+    self.filteredEvent = self.events;
+    [self.tableView reloadData];
+}
+
+//- (void)NSString *)searchText {
+//    if (searchText.length != 0) {
+//        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSDictionary *evaluatedObject, NSDictionary *bindings) {
+//            return [event containsString:searchText] || [type containsString:searchText];
+//        }];
+//        self.filteredEvent = [self.events filteredArrayUsingPredicate:predicate];
+//        NSLog(@"%@", self.filteredEvent);
+//    }
+//    else {
+//        self.filteredEvent = self.events;
+//    }
+//    [self.tableView reloadData];
+//}
+
 - (IBAction)didSearch:(id)sender {
     SceneDelegate *sceneDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
 
@@ -145,7 +235,6 @@
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
         // PFUser.current() will
     }];
-    
 }
 
-    @end
+@end
--- HangOut/SearchViewController.m
+++ HangOut/SearchViewController.m
@@ -1,74 +1,105 @@
+//////
+//////  SearchViewController.m
+//////  HangOut
+//////
+//////  Created by Oluwanifemi Kolawole on 7/5/22.
+//////
 ////
-////  SearchViewController.m
-////  HangOut
+//#import "SearchViewController.h"
+//#import "Parse/Parse.h"
+//#import "EventCell.h"
+//#import "SearchCell.h"
+//#import "EventsViewController.h"
 ////
-////  Created by Oluwanifemi Kolawole on 7/5/22.
-////
+//@interface SearchViewController ()
 //
-#import "SearchViewController.h"
-#import "Parse/Parse.h"
-#import "EventCell.h"
-#import "SearchCell.h"
+//@property (strong, nonatomic) UISearchController *searchController;
+//@property (nonatomic, strong) NSArray *events;
 //
-@interface SearchViewController ()
-
-@property (strong, nonatomic) UISearchController *searchController;
-@property (strong, nonatomic)  NSArray *filteredData;
-
-@end
-
-@implementation SearchViewController
-NSArray *data;
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    data = @[@"New York, NY", @"Los Angeles, CA", @"Baton Rouge, LA", @"Houston, TX",
-                  @"Oslo, Norway", @"Phoenix, AZ", @"Albuquerque, NM", @"San Antonio, TX",
-                  @"Dallas, TX", @"Cincinnati, OH", @"San Jose, CA", @"Indianapolis, IN",
-                  @"JLas Vegas, NV", @"San Francisco, CA", @"Columbus, OH", @"Austin, TX",
-                  @"Memphis, TN", @"Baltimore, MD", @"Charlotte, ND", @"Nashville, TN", @"Shipshewana, IN", @"Hermosa Beach, CA"];
-    
-    self.tableView.dataSource = self;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:nil];
-       cell.textLabel.text = data[indexPath.row];
-       return  cell;
-    
-}
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return data.count;
-}
-
-- (void)updateSearchResultsForSearchController:(nonnull UISearchController *)searchController {
-}
-
-- (void)encodeWithCoder:(nonnull NSCoder *)coder {
-}
-
-- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
-}
-
-- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-}
-
-- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
-}
-
-- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-}
-
-- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
-}
-
-- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
-}
-- (void)setNeedsFocusUpdate {
-}
-
-- (void)updateFocusIfNeeded {
-}
-
-@end
+//@end
+//
+//@implementation SearchViewController
+//NSArray *data;
+//NSArray *filteredData;
+//NSString *CellIdentifier = @"EventCell";
+//
+//- (void)viewDidLoad {
+//    [super viewDidLoad];
+//
+//    data = @[@"New York, NY", @"Los Angeles, CA", @"Baton Rouge, LA", @"Houston, TX",
+//                  @"Oslo, Norway", @"Phoenix, AZ", @"Albuquerque, NM", @"San Antonio, TX",
+//                  @"Dallas, TX", @"Cincinnati, OH", @"San Jose, CA", @"Indianapolis, IN",
+//                  @"JLas Vegas, NV", @"San Francisco, CA", @"Columbus, OH", @"Austin, TX",
+//                  @"Memphis, TN", @"Baltimore, MD", @"Charlotte, ND", @"Nashville, TN", @"Shipshewana, IN", @"Hermosa Beach, CA"];
+//
+//    self.tableView.dataSource = self;
+//    self.searchBar.delegate = self;
+//    self.tableView.delegate = self;
+//
+//    filteredData = data;
+//    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
+//
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    return filteredData.count;
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+//    cell.textLabel.text =  filteredData[indexPath.row];
+////    cell.locationLabel.text = venue[@"display_location"];
+//       return  cell;
+//
+//}
+//
+//- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+//
+//    if (searchText.length != 0) {
+//
+//        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString *evaluatedObject, NSDictionary *bindings) {
+            
+//            return [evaluatedObject containsString:searchText];
+//        }];
+//        filteredData = [data filteredArrayUsingPredicate:predicate];
+//
+//        NSLog(@"%@", filteredData);
+//
+//    }
+//    else {
+//        filteredData = data;
+//    }
+//
+//    [self.tableView reloadData];
+//
+//}
+//- (void)updateSearchResultsForSearchController:(nonnull UISearchController *)searchController {
+//}
+//
+//- (void)encodeWithCoder:(nonnull NSCoder *)coder {
+//}
+//
+//- (void)traitCollectionDidChange:(nullable UITraitCollection *)previousTraitCollection {
+//}
+//
+//- (void)preferredContentSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+//}
+//
+//- (void)systemLayoutFittingSizeDidChangeForChildContentContainer:(nonnull id<UIContentContainer>)container {
+//}
+//
+//- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+//}
+//
+//- (void)willTransitionToTraitCollection:(nonnull UITraitCollection *)newCollection withTransitionCoordinator:(nonnull id<UIViewControllerTransitionCoordinator>)coordinator {
+//}
+//
+//- (void)didUpdateFocusInContext:(nonnull UIFocusUpdateContext *)context withAnimationCoordinator:(nonnull UIFocusAnimationCoordinator *)coordinator {
+//}
+//- (void)setNeedsFocusUpdate {
+//}
+//
+//- (void)updateFocusIfNeeded {
+//}
+//
+//@end

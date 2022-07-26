3779ddbac6d37e7793c2c14e3b1c97b7549558ac
Grace Kolawole <107007242+GraceKolawole@users.noreply.github.com>
2022-07-26 12:16:42 -0700

    Add the locationsViewContoller to the Map folder and added the Client_Secret and Client_ID to the Keys.plist

---
+++ HangOut/Map /LocationsViewController.h
@@ -0,0 +1,16 @@
+//
+//  LocationsViewController.h
+//  HangOut
+//
+//  Created by Oluwanifemi Kolawole on 7/26/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationsViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
